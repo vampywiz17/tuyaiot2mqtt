12dd7dc45f2c8fba9541dffadfade38ce13fd023
Vamp <14281430+vampywiz17@users.noreply.github.com>
2022-01-10 18:02:01 +0100

    convert broker port to integer

--- Docker/tuyaiot2mqtt.py
+++ Docker/tuyaiot2mqtt.py
@@ -22,10 +22,12 @@ open_pulsar = TuyaOpenPulsar(
     ACCESS_ID, ACCESS_KEY, MQ_ENDPOINT, TuyaCloudPulsarTopic.PROD
 )
 
+BROKER_PORT_INT = int(BROKER_PORT)
+
 client = mqtt.Client(CLIENT_NAME)
 client.username_pw_set(username=USERNAME,password=PASSWORD)
-
-open_pulsar.add_message_listener(lambda msg: (client.connect(BROKER_ADDRESS, BROKER_PORT), client.publish(TOPIC,msg)))
+BROKER_PORT_INT = int(BROKER_PORT)
+open_pulsar.add_message_listener(lambda msg: (client.connect(BROKER_ADDRESS, BROKER_PORT_INT), client.publish(TOPIC,msg)))
 
 # Start Message Queue
 open_pulsar.start()
